752c0456a6ee0d677a58af754940c314a88efba7
Theodoros90 <ionas90@hotmail.com>
2025-09-04 11:03:58 +0300

    End of section 6

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,11 +5,18 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 import App from './app/layout/App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
+
   </StrictMode>,
 )
