bdb99737e781aedbec1cb35952dd6ce2aedab137
Theodoros90 <ionas90@hotmail.com>
2025-09-08 14:23:57 +0300

    End of section 7 NEW

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,19 +1,20 @@
 import { Box } from "@mui/material";
 import ActivityCard from "./ActivityCard";
+import { useActivities } from "../../../lib/hooks/useActivities";
+import Typography from "@mui/material/Typography";
 
-type Props = {
-    activities: Activity[]
-    selectActivity: (id: string) => void;
-}
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+
+export default function ActivityList() {
+    const { activities, isPending } = useActivities();
+
+    if (!activities || isPending) return <Typography>Loading...</Typography>
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
             {activities.map(activity => (
                 <ActivityCard
                     key={activity.id}
                     activity={activity}
-                    selectActivity={selectActivity}
                 />
             ))}
         </Box>
