ad56917b517153dae4cac1bca449dad30ea23a7b
Theodoros90 <ionas90@hotmail.com>
2025-09-09 11:03:14 +0300

    End of Section 8 V-2

--- client/src/features/activities/details/ActivityDetailPage.tsx
+++ client/src/features/activities/details/ActivityDetailPage.tsx
@@ -1,10 +1,12 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { Link, useParams } from "react-router";
-import { useNavigate } from "react-router";
+import { Grid2, Typography } from "@mui/material"
+import { useParams } from "react-router";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import ActivityDetailsHeader from "./ActivityDetailsHeader";
+import ActivityDetailsInfo from "./ActivityDetailsInfo";
+import ActivityDetailsChat from "./ActivityDetailsChat";
+import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
 
 export default function ActivityDetailPage() {
-    const navigate = useNavigate();
     const { id } = useParams();
     const { activity, isLoadingActivity } = useActivities(id);
 
@@ -13,20 +15,15 @@ export default function ActivityDetailPage() {
     if (!activity) return <Typography>Activity not found</Typography>
 
     return (
-        <Card sx={{ borderRadius: 3 }}>
-            <CardMedia
-                component='img'
-                src={`/images/categoryImages/${activity.category}.jpg`}
-            />
-            <CardContent>
-                <Typography variant="h5">{activity.title}</Typography>
-                <Typography variant="subtitle1" fontWeight='light'>{activity.date}</Typography>
-                <Typography variant="body1">{activity.description}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button component={Link} to={`/manage/${activity.id}`} color="primary">Edit</Button>
-                <Button onClick={() => navigate('/activities')} color="inherit">Cancel</Button>
-            </CardActions>
-        </Card>
+        <Grid2 container spacing={3}>
+            <Grid2 size={8}>
+                <ActivityDetailsHeader activity={activity} />
+                <ActivityDetailsInfo activity={activity} />
+                <ActivityDetailsChat />
+            </Grid2>
+            <Grid2 size={4}>
+                <ActivityDetailsSidebar />
+            </Grid2>
+        </Grid2>
     )
 }
